b8162a6618d478005de6bf973caa869b592f45bd
Michelle Poon <michelle@nokacreative.com>
2021-09-28 22:40:42 -0600

    - Changed rollup-plugin-sass to rollup-plugin-scss    -> Removed rollup-plugin-import-css

--- rollup.config.js
+++ rollup.config.js
@@ -2,11 +2,10 @@ import peerDepsExternal from 'rollup-plugin-peer-deps-external'
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import typescript from 'rollup-plugin-typescript2'
-import sass from 'rollup-plugin-sass'
+import scss from 'rollup-plugin-scss'
 import json from '@rollup/plugin-json'
 import autoprefixer from 'autoprefixer'
 import postcss from 'postcss'
-import css from 'rollup-plugin-import-css'
 
 const packageJson = require('./package.json')
 
@@ -29,8 +28,10 @@ export default {
     resolve(),
     commonjs(),
     typescript({ useTsconfigDeclarationDir: true }),
-    sass({
-      output: 'dist/bundle.css',
+    scss({
+      failOnError: true,
+      outputStyle: 'compressed',
+      prefix: `@use '../../../assets/mixins' as *;`,
       processor: (css) =>
         postcss({
           extensions: ['scss'],
@@ -39,7 +40,6 @@ export default {
           .process(css)
           .then((result) => result.css),
     }),
-    css({ minify: true }),
     json(),
   ],
   external: ['react', 'react-dom'],
